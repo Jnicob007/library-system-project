e46f007ebc762520309f3743d0c0f40a33872ef6
Jnicob <mail@mail.com>
2025-11-17 09:06:12 +0100

    17.11.2025

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,6 @@ public class Main{
         Publication p;
         PublicationDB pDB;
 
-        // aby obsłużyć program, do interfejsu z fabryką przypisujemy odpowiednią fabrykę, np. AudiobookFactory
-        // następnie do zmiennych p oraz pDB przypisujemy odpowiedni obiekt publikacji oraz obiekt do obslugi operacji na bazie danych
-        // program sam wykrywa odpowiednie typy obiektów przez wczesniejsze przypisanie odpowiedniej fabryki do zmiennej factory
-        factory = new AudiobookFactory();
-        p = factory.createPublication();
-        pDB = factory.createPublicationDB();
-        p.displayInfo();
-        pDB.addPublication(p);
-        pDB.deletePublication(3);
 
         // tutaj menu jakies ze switchem
         Scanner scanner = new Scanner(System.in);
